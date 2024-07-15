8aa94ade2d990848049d2514b9f4845e93c32663
Mac <mac@Macs-MacBook-Pro.local>
2024-07-15 19:22:33 +0100

    factorial of a number

---
+++ index.js
@@ -0,0 +1,16 @@
+// Give an integer 'n', find the factorial of that integer 
+function factorial (n) {
+   let fact =  1;
+    for(let i = 2; i <= n; i++){
+        fact *= i
+    }
+    return fact
+}
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(5));
+console.log(factorial(20));
+console.log(factorial(2));
+
+// Big O = O(n)
